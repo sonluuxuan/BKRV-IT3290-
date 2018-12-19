26992f9f3e828de955e829818e5f07ef6b529e6a
Duc Tran Vu <vuductran153@gmail.com>
2018-12-19 22:30:11 +0700

    finish post.html

---
+++ testpost.php
@@ -0,0 +1,7 @@
+<?php
+	$x = $_POST['storeName'];
+	$y = $_POST['storeLocation'];
+	$z = $_POST['storeType'];
+	$w = $_POST['storeArea'];
+	echo $x;
+?>
--- upload_file.php
+++ upload_file.php
@@ -1,12 +1,26 @@
 <?php
-if(isset($_POST['submit_image']))
-{
+include 'connection.php';
+// get review id for forder name //
+$query = "SELECT id FROM review WHERE id >= ALL(SELECT id FROM review)";
+$result = mysqli_query($conn, $query);
+$row = mysqli_fetch_assoc($result);
+$id = $row['id'];
+$path = "images/".$id; // relative path //
+if (!file_exists($path)) {
+    mkdir($path, 0777, true);
+} // create folder //
+
+// upload and move file to destination folder //
 for($i=0;$i<count($_FILES["upload_file"]["name"]);$i++)
 {
  $uploadfile=$_FILES["upload_file"]["tmp_name"][$i];
- $folder="images/";
- move_uploaded_file($_FILES["upload_file"]["tmp_name"][$i], "$folder".$_FILES["upload_file"]["name"][$i]);
+ move_uploaded_file($_FILES["upload_file"]["tmp_name"][$i], "$path"."/".$_FILES["upload_file"]["name"][$i]);
 }
 exit();
-}
+
+// handle the remaining queries here //
+
+//
+
+mysqli_close($conn);
 ?>
