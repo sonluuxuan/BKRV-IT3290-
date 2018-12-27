2b20c080f9958a1e686b15cb1d47423ea3c7c051
Duc Tran Vu <vuductran153@gmail.com>
2018-12-27 21:12:45 +0700

    BKRV v1.1

--- register.php
+++ register.php
@@ -1,20 +1,3 @@
-<?php
-if(isset($_GET['error'])){
-	$error = $_GET['error'];
-}
-?>
-<script type="text/javascript">
-	var error = "";
-	error = "<?php echo "$error";?>";
-	if(error != ""){
-		if(error == "passwordnotmatched")
-			alert("Mật khẩu và xác nhận mật khẩu không khớp!");
-		else if(error == "userexisted")
-			alert("Tên đăng nhập đã tồn tại!\nVui lòng chọn tên đăng nhập khác!");
-		else if(error == "EmailExisted")
-			alert("Địa chỉ email đã tồn tại!\nVui lòng chọn địa chỉ khác!");
-	}
-</script>
 <!DOCTYPE html>
 <html lang="en" >
 
@@ -29,8 +12,8 @@ if(isset($_GET['error'])){
 <link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css'>
 
 			<link rel="stylesheet" href="css/login-register.css">
+			<script src="jquery-3.2.1.min.js"></script>
 
-	
 </head>
 
 <body>
@@ -55,28 +38,138 @@ if(isset($_GET['error'])){
 	<div class="signup__container">
 	<div class="container__child signup__thumbnail">
 	</div>
+
+	<script type="text/javascript" src="js/jquery-3.3.1.min.js"></script>
+	<script type="text/javascript">
+		var username;
+		var password;
+		var passwordRepeat;
+		var isAvailable = true;
+
+		function checkusername()
+		{
+			$(document).ready(function(){		
+				username = $('#username').val();
+				$.ajax({
+					url:"checkUsername.php",
+					method:"POST",
+					data:{user_name:username},
+					dataType:"text",
+					success:function(html){						
+						$('#username_span').html(html);	
+					}
+				});
+			});
+		}
+
+		function checkemail()
+		{
+			$(document).ready(function(){		
+				email = $('#email').val();
+				$.ajax({
+					url:"checkUsername.php",
+					method:"POST",
+					data:{email_ajax:email},
+					dataType:"text",
+					success:function(html){						
+						$('#email_span').html(html);	
+					}
+				});
+			});
+		}
+
+		$(document).ready(function(){
+
+			$('#password').blur(function(){
+				password = $(this).val();
+
+				var passhtmlString = '';
+				isAvailable = true;
+
+				$('#password_span').html('');
+
+				if (password.length < 7) 
+				{
+					passhtmlString += 'Ngắn ';
+					isAvailable = false;
+				}
+
+				if (!password.match(/([a-z].*[A-Z])|([A-Z].*[a-z])/)) 
+				{
+					passhtmlString += 'Chữ hoa đâu ';
+					isAvailable = false;
+				}
+
+				if (!password.match(/([0-9])/)) 
+				{
+					passhtmlString += 'Số đâu ';
+					isAvailable = false;
+				}
+
+				if (!password.match(/([!,%,&,@,#,$,^,*,?,_,~])/)) 
+				{
+					passhtmlString += 'KTDB đâu ';
+					isAvailable = false;			
+				}
+
+				$('#password_span').html('<span style="font-size:10px;"><font color="red">' + passhtmlString + '</font></span>');
+			});
+
+			$('#passwordRepeat').blur(function(){
+
+				$('#passwordRepeat_span').html('');
+				var passRepString = '';
+				passwordRepeat = $(this).val();
+				isAvailable = true;
+
+				if(password != passwordRepeat) 
+				{
+					passRepString += 'pát # nhau';
+					isAvailable = false;
+				}
+				else 
+				{
+					passRepString += 'ok men';
+					isAvailable = true;					
+				}
+
+				$('#passwordRepeat_span').html('<span style="font-size:10px;"><font color="red">' + passRepString + '</font></span>');
+			});
+
+			$('#submit').click(function(){
+				if(isAvailable == true)
+				{
+					$('#formId').attr('action', "signup.php");
+				}
+			});
+		});
+	</script>
 	<div class="container__child signup__form">
-		<form action="http://localhost/webservice/project_csdl/signup.php" method="post">
+		<form action = '#' id = "formId" method="post">
 			<div class="form-group">
 				<label for="username">Tên đăng nhập</label>
-				<input class="form-control" type="text" name="username" id="username" placeholder="username" required />
+				<input class="form-control" type="text" name="username" id="username" onblur = "checkusername();" placeholder="username" required />
+				<span id = "username_span" class = "span"></span>
 			</div>
 			<div class="form-group">
 				<label for="email">Email</label>
-				<input class="form-control" type="text" name="email" id="email" placeholder="user.name@gmail.com" required />
+				<input class="form-control" type="text" name="email" id="email" onblur = "checkemail();" placeholder="user.name@gmail.com" required />
+				<span id = "email_span" class = "span"></span>
 			</div>
 			<div class="form-group">
 				<label for="password">Mật khẩu</label>
 				<input class="form-control" type="password" name="password" id="password" placeholder="********" required />
+				<span id = "password_span" class = "span"></span>
 			</div>
 			<div class="form-group">
 				<label for="passwordRepeat">Xác nhận mật khẩu</label>
 				<input class="form-control" type="password" name="passwordRepeat" id="passwordRepeat" placeholder="********" required />
+				<span id = "passwordRepeat_span" class = "span"></span>
 			</div>
-			<div class="m-t-lg">
+			<div class="m-t-lgl">
 				<ul class="list-inline">
 					<li>
-						<input class="btn btn--form" type="submit" name="submit" value="Đăng ký" />
+						<input class="btn btn--form" type="submit" name="submit" id="submit" value="Đăng ký" />
 					</li>
 					<li>
 						<a class="signup__link" href="login.php">Đã là thành viên?</a>
@@ -88,6 +181,8 @@ if(isset($_GET['error'])){
 </div>
 
 <!-- JavaScript -->
+<script src="js/jquery-3.2.1.min.js"></script>
+
 <script src="js/jquery-1.12.1.min.js"></script>
 <script src="js/bootstrap.min.js"></script>
 
