c8e72b0a66b7e433c9fd95f3255fe324f367b578
sonluuxuan <luuxuansond@gmail.com>
2019-06-30 00:16:23 +0700

    remove conflict

--- BKRV/subscribe.php
+++
@@ -1,7 +0,0 @@
-<?php
-include "DB_functions.php";
-$posterId = $_POST['posterId'];
-$subscriberId = $_POST['subscriberId'];
-$result = add_subscriber($posterId, $subscriberId);
-echo json_encode($result);
-// echo "worked";
